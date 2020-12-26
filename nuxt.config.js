a973aa881b077e0ca16cb5b2cfd1edc8e9a9f126
Sagiv Bonali <sagivb13@gmail.com>
2020-12-26 23:08:13 +0200

    fix bugs

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,6 +42,7 @@ export default {
     '@nuxtjs/onesignal',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
+    "@nuxtjs/dotenv",
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
