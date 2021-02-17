06c3305607347cab77f58b065fe650e394604737
Sagiv Bonali <sagivb13@gmail.com>
2021-02-17 20:51:47 +0200

    check normal form

--- pages/contact-Us.vue
+++ pages/contact-Us.vue
@@ -54,6 +54,31 @@ v-card(elevation="14")
     v-spacer 
     v-btn(@click="handleSubmit", type="submit") submit the form
 </template>
+
+
+<template>
+    <form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+
+</template>
 <script>
 export default {
   data: () => ({
