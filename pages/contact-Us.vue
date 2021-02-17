6936e9132c33514124114832e58a29e0d648096f
Sagiv Bonali <sagivb13@gmail.com>
2021-02-17 18:05:38 +0200

    comit

--- pages/contact-Us.vue
+++ pages/contact-Us.vue
@@ -1,6 +1,15 @@
 <template lang="pug">
-    v-form( name="contact" netlify data-netlify-honeypot='bot-field' action="/" method="post")
-        v-text-field( v-model="email" name="email" )
+    v-card( hover max-width="500" )
+        v-card-title form to contact
+        v-card-text 
+            v-form( name="contact" netlify data-netlify-honeypot='bot-field' action="/" method="post")
+                v-text-field( v-model="email" name="email" )
+        v-card-actions
+            v-btn( type="submit"  ) submit
+                
+        
+    
+
 </template>
 <script>
 export default {
