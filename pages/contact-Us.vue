fbe3bdbcbd080156bc1dfa988a94293ba9bf7d24
Sagiv Bonali <sagivb13@gmail.com>
2021-02-17 18:01:06 +0200

    send form

--- pages/contact-Us.vue
+++ pages/contact-Us.vue
@@ -1,13 +1,11 @@
-<template>
-  
+<template lang="pug">
+    v-form( name="contact" netlify data-netlify-honeypot='bot-field' action="/" method="post")
+        v-text-field( v-model="email" name="email" )
 </template>
-
 <script>
 export default {
-
+    data:()=>({
+        email: null
+    })
 }
 </script>
-
-<style>
-
-</style>
