cdc1c7186c4f9c4deab901929e391f1a9e69c154
Sagiv Bonali <sagivb13@gmail.com>
2021-02-17 21:05:01 +0200

    fix form bugs

--- pages/contact-Us.vue
+++ pages/contact-Us.vue
@@ -54,31 +54,6 @@ v-card(elevation="14")
     v-spacer 
     v-btn(@click="handleSubmit", type="submit") submit the form
 </template>
-
-
-<template>
-    <form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
-
-</template>
 <script>
 export default {
   data: () => ({
@@ -110,7 +85,7 @@ email: null,
       ...this.form
     })
     
-  }).then(() => alert("hoof!")).catch(error => alert(error))
+  }).then(() => this.$swal('we got your messege', `we back to ${form.email} mail`, success)).catch(error => this.$swal('error', error, error))
 
   
     },
